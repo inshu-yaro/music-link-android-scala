844de338f5355af6eeaadcc6e00d5f7258cda78f
Kazuya Uesato <kazuya@kazuya-CF-S10AY2DC.(none)>
2014-06-15 05:01:12 +0900

    login

--- src/com/mlink/mlink/managers/FacebookLoginManager.scala
+++ src/com/mlink/mlink/managers/FacebookLoginManager.scala
@@ -1,40 +1,28 @@
-package com.mlink.mlink.activities
+package com.mlink.mlink.managers
 
-import org.scaloid.common.SActivity
-import com.facebook.Session.{StatusCallback, OpenRequest}
-import _root_.android.view.textservice.{SuggestionsInfo, TextInfo}
-import _root_.android.service.textservice.SpellCheckerService.Session
-import com.facebook
-import com.facebook._
-import com.facebook.model.GraphUser
-import _root_.android.app.{Activity, ProgressDialog}
-import _root_.android.util.Log
-import _root_.android.os.{AsyncTask, Bundle}
-import com.facebook.android.{FacebookError, DialogError, Facebook}
+import com.facebook.android.{DialogError, FacebookError, Facebook}
 import com.facebook.android.Facebook.DialogListener
-import _root_.android.content.Intent
+import android.os.Bundle
+import scala.concurrent._
 import org.apache.http.client.HttpClient
 import org.apache.http.impl.client.DefaultHttpClient
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.{HttpEntity, HttpResponse}
-import java.io.{InputStreamReader, BufferedReader, InputStream}
-import com.mlink.mlink.util.Logger
-import _root_.android.net.Uri
+import java.io.InputStream
 import org.apache.commons.io.IOUtils
-import org.json.JSONArray
-import scala.concurrent.future
-import scala.concurrent.ExecutionContext.Implicits.global
-import com.facebook.Request.Callback
+import android.content.Intent
+import org.scaloid.common.SActivity
 
 /**
- * Created by kazuya on 14/06/14.
+ * Created by kazuya on 14/06/15.
  */
-class AuthorizationActivity extends SActivity with Logger {
+class FacebookLoginManager {
+  val APP_ID = "735435546497941"
+  val facebook: Facebook = new Facebook(APP_ID)
 
-  val facebook: Facebook = new Facebook("735435546497941")
-  onCreate{
+  def login(activity:SActivity){
     if (!facebook.isSessionValid) {
-      facebook.authorize(this, new DialogListener {
+      facebook.authorize(activity, new DialogListener {
         override def onComplete(p1: Bundle): Unit = {
           //new AsyncHttpRequest(null).execute()
 
@@ -114,38 +102,37 @@ class AuthorizationActivity extends SActivity with Logger {
     super.onActivityResult(requestCode, resultCode, data)
     facebook.authorizeCallback(requestCode, resultCode, data)
   }
-/*  private def loginFacebook() {
-    var openRequest : OpenRequest = new OpenRequest(this).setCallback(mFacebookCallback)
-    openRequest.setPermissions()
-   }
+  /*  private def loginFacebook() {
+      var openRequest : OpenRequest = new OpenRequest(this).setCallback(mFacebookCallback)
+      openRequest.setPermissions()
+     }
 
-  private val mFacebookCallback : facebook.Session.StatusCallback = new StatusCallback {
-    override def call(session: facebook.Session, state: SessionState, exception: Exception) {
-      onSessionStateChange(session, state, exception)
+    private val mFacebookCallback : facebook.Session.StatusCallback = new StatusCallback {
+      override def call(session: facebook.Session, state: SessionState, exception: Exception) {
+        onSessionStateChange(session, state, exception)
+      }
     }
-  }
 
-  private def onSessionStateChange(session:facebook.Session, state:SessionState, exception:Exception){
-    if (session.isOpened()){
-      Request.newMeRequest(session, new FacebookGraphUserCallback("wait ...."){
-        override def onCompleted(user:GraphUser, response:Response){
-          super.onCompleted(user, response)
-          Log.d("AutholizedActivity", user.getInnerJSONObject().toString())
-        }
-      }).executeAsync()
+    private def onSessionStateChange(session:facebook.Session, state:SessionState, exception:Exception){
+      if (session.isOpened()){
+        Request.newMeRequest(session, new FacebookGraphUserCallback("wait ...."){
+          override def onCompleted(user:GraphUser, response:Response){
+            super.onCompleted(user, response)
+            Log.d("AutholizedActivity", user.getInnerJSONObject().toString())
+          }
+        }).executeAsync()
+      }
     }
-  }
 
-  class FacebookGraphUserCallback(message:String) extends Request.GraphUserCallback{
-    private var mProgress : ProgressDialog = null
-    mProgress = new ProgressDialog(AutholizationActivity.this)
-    mProgress.setMessage(message)
-    mProgress.setProgressStyle(ProgressDialog.STYLE_SPINNER)
-    mProgress.show()
+    class FacebookGraphUserCallback(message:String) extends Request.GraphUserCallback{
+      private var mProgress : ProgressDialog = null
+      mProgress = new ProgressDialog(AutholizationActivity.this)
+      mProgress.setMessage(message)
+      mProgress.setProgressStyle(ProgressDialog.STYLE_SPINNER)
+      mProgress.show()
 
-    override def onCompleted(user:GraphUser, response:Response){
-      mProgress.dismiss()
-    }
-  }*/
-
+      override def onCompleted(user:GraphUser, response:Response){
+        mProgress.dismiss()
+      }
+    }*/
 }
