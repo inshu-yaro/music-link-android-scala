924b9a9f300192366fc7cd2efcb4ecb49d3d7878
Kazuya Uesato <kazuya@kazuya-CF-S10AY2DC.(none)>
2014-06-15 15:45:01 +0900

    alert

--- src/com/mlink/mlink/managers/NotificatoinManager.scala
+++ src/com/mlink/mlink/managers/NotificatoinManager.scala
@@ -1,22 +1,24 @@
 package com.mlink.mlink.managers
 
-import org.scaloid.common.SActivity
+import org.scaloid.common.{SIntent, SActivity}
 import android.app.AlertDialog
 import android.app.AlertDialog.Builder
-import android.content.DialogInterface
+import android.content.{Context, Intent, DialogInterface}
 import android.content.DialogInterface.OnClickListener
 import com.mlink.mlink.R
 import android.view.ContextThemeWrapper
+import com.mlink.mlink.activities.{ChatActivity, UserLoginActivity}
+import org.scaloid.common.Implicits._
 
 /**
  * Created by kazuya on 14/06/15.
  */
-class NotificatoinManager (activity:SActivity) {
+class NotificatoinManager (implicit val ctx:Context) {
   def mNotify() = {
-    new Builder(new ContextThemeWrapper(activity, R.style.AlertDialog)).setTitle("マッチングしました。")
+    new Builder(new ContextThemeWrapper(ctx, R.style.AlertDialog)).setTitle("マッチングしました。")
     .setPositiveButton("確認",new OnClickListener {
       override def onClick(p1: DialogInterface, p2: Int): Unit = {
-        //go Matching Activity
+        ctx.startActivity(SIntent[ChatActivity])
       }
     }).setNegativeButton("保留", new OnClickListener {
       override def onClick(p1: DialogInterface, p2: Int): Unit = {
